c36095e605960dfece9969da16b2fc6b980ccbf3
OriolVila <oriolvm@gmail.com>
2022-02-06 12:50:55 +0100

    Polishing code flatmates

---
+++ App-2-Flatmates-Bill/reports.py
@@ -0,0 +1,48 @@
+import webbrowser
+import fpdf
+import os
+
+class PdfReport:
+    """
+    Creates a pdf file that contains data about the flatmates
+    such as their names, their due amounts and the period of the bill.
+    """
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate(self, flatmate1, flatmate2, bill):
+        
+        flatmate1_pay = str(round(flatmate1.pays(bill, flatmate2), 2)) + " euros"
+        flatmate2_pay = str(round(flatmate2.pays(bill, flatmate1), 2)) + " euros"
+
+        pdf = fpdf.FPDF(orientation="P", unit="pt", format="A4")
+        pdf.add_page()
+
+        #Ad icon 
+        pdf.image("files/house.png", w=30, h=30)
+
+        #Add title
+        pdf.set_font(family="Times", size=24, style="B")
+        pdf.cell(w=0, h=80, txt="Bill Report", border=0, align="C", ln=1)
+
+        #Insert Period label
+        pdf.set_font(family="Times", size=14, style="B")
+        pdf.cell(w=100, h=40, txt="Period:", border=0)
+        pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
+
+        #Insert name and amount flatmate1
+        pdf.set_font(family="Times", size=14)
+        pdf.cell(w=100, h=25, txt=flatmate1.name, border=0)
+        pdf.cell(w=150, h=25, txt=flatmate1_pay, border=0, ln=1)
+
+        #Insert name and amount flatmate1
+        pdf.cell(w=100, h=25, txt=flatmate2.name, border=0)
+        pdf.cell(w=150, h=25, txt=flatmate2_pay, border=0, ln=1)
+
+        #Save bill.pdf
+        pdf.output(f"files/{self.filename}")
+
+        #Alternative way to change directory
+        os.chdir("files")
+        webbrowser.open(self.filename)
